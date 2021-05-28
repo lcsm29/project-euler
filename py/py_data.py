c509ea485a9208c9394ff543850e6e098c878a22
lcsm29 <lcsm29@outlook.com>
2021-05-29 08:24:03 +0900

    problem 2 updated

--- py/py_data.py
+++ py/py_data.py
@@ -1,15 +1,19 @@
 file_names = {
     1: 'py_1_multiples_of_3_and_5',
+	2: 'py_2_even_fibonacci_numbers',
 }
 
 ans = {
 	  1: 233168,
+	  2: 4613732,
 }
 
 var = {
 	1: 1000,
+	2: 4000000,
 }
 
 iters = {
 	1: 10000,
+	2: 10000,
 }
