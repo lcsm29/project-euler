f329243d698522fe4fcbce42754e5a127c38a8b1
lcsm29 <lcsm29@outlook.com>
2021-05-29 08:17:47 +0900

    main.py added main.py runs every solutions appeared on py_data.py (file_names); you can set the number of iterations on py_data.py (iters), but the default value is acceptable.
    it also displays;
    a) total computing time for each function on each solution
    b) min, avg, max value for the each round of the iteration
    c) fastest, slowest function
    d) error count (if there is any)

---
+++ py/main.py
@@ -0,0 +1,71 @@
+import statistics as stat
+import time
+import importlib
+from py_data import *
+
+
+def call_everything(i, *var):
+    global num_fn
+    fn_lst = [v for f, v in funcs.__dict__.items() if f.startswith('fn_')]
+    result = {str(f)[10:str(f).index(' at ')] + '()': 0 for f in fn_lst}
+    for f in fn_lst:
+        tmp = []
+        outer = time.perf_counter_ns()
+        for _ in range(i):
+            s = time.perf_counter_ns()
+            f.__call__(var[0])
+            tmp.append(time.perf_counter_ns() - s)
+        result[str(f)[10:str(f).index(' at ')] + '()'] = (
+            time.perf_counter_ns() - outer,
+            min(tmp), stat.mean(tmp), max(tmp)
+        )
+        num_fn += 1
+    return result
+
+
+def comparo(result):
+    fastest = {'overall': [float('inf'), ''], 'min': [float('inf'), ''],
+               'avg':     [float('inf'), ''], 'max': [float('inf'), '']}
+    slowest = {'overall': [0, ''], 'min': [0, ''],
+               'avg':     [0, ''], 'max': [0, '']}
+    for func, ns in result.items():
+        print(f"{func} time elapsed: {ns[0]:,}ns "
+              f"(min {ns[1]:,}ns, avg {ns[2]:,.0f}ns, max {ns[3]:,}ns)")
+        for i, key in enumerate(fastest.keys()):
+            if min(fastest[key][0], ns[i]) == ns[i]:
+                fastest[key] = [ns[i], func]
+            if max(slowest[key][0], ns[i]) == ns[i]:
+                slowest[key] = [ns[i], func]
+    for f, s in zip(fastest.items(), slowest.items()):
+        print(f'fastest {f[0]}: {f[1][1]} took {f[1][0]:,.0f}ns')
+        print(f'slowest {s[0]}: {s[1][1]} took {s[1][0]:,.0f}ns'
+              f' ({s[1][0]/f[1][0]:.2f} times slower)')
+
+
+def validate_result_once(i, *var):
+    fn_lst = [v for f, v in funcs.__dict__.items() if f.startswith('fn_')]
+    correct_answer = ans[i]
+    num_wrong = 0
+    for f in fn_lst:
+        if f.__call__(var[0]) != correct_answer:
+            num_wrong += 1
+    print(f"Number of wrong answers: {num_wrong}\n")
+    return num_wrong
+
+
+if __name__ == '__main__':
+    fastest_overall = {}
+    wrong_answers = {}
+    num_fn = 0
+    for k in file_names.keys():
+        print(f'Calculating Project Euler ID {k} ({iters[k]:,} times): ')
+        funcs = importlib.import_module(file_names[k], package=None)
+        comparo(call_everything(iters[k], var[k]))
+        num_wrong = validate_result_once(k, var[k])
+        if num_wrong:
+            wrong_answers[k] = num_wrong
+    if len(wrong_answers):
+        for k, v in wrong_answers.items():
+            print(f"ID {k}({file_names[k]}): {v} function(s) failed")
+    else:
+        print(f"0/{num_fn} functions failed")
---
+++ py/py_data.py
@@ -0,0 +1,15 @@
+file_names = {
+    1: 'py_1_multiples_of_3_and_5',
+}
+
+ans = {
+	  1: 233168,
+}
+
+var = {
+	1: 1000,
+}
+
+iters = {
+	1: 10000,
+}
