760254bb92d9a15db64980c5f34d12f938b0eee9
lcsm29 <lcsm29@outlook.com>
2021-05-31 05:32:44 +0900

    solution for problem 5 added

--- py/py_0005_smallest_multiple.py
+++ py/py_0005_smallest_multiple.py
@@ -8,14 +8,40 @@
 # 
 # by lcsm29 http://github.com/lcsm29/project-euler
 import timed
+from math import prod
 
+''' removed beceause it is too slow
+def fn_brute(n):
+    while n:
+        n += 2
+        for i in range(3, 21):
+            if n % i != 0:
+                break
+            if i == 20 and n % i == 0:
+                return n
+'''
 
-def dummy(n):
-    pass
-
+def fn_prime_based(n):
+    def get_prime_factors(num):
+        factors_counts = {i: 0 for i in range(1, n + 1)}
+        i, divided = 2, num
+        while i ** 2 <= divided:
+            if divided % i:
+                i += 1
+            else:
+                factors_counts[i] += 1
+                divided //= i
+        factors_counts[divided] += 1
+        return factors_counts
+    factors = {i: 0 for i in range(1, n + 1)}
+    for i in range(1, n + 1):
+        for k, v in get_prime_factors(i).items():
+            if factors[k] < v:
+                factors[k] = v
+    return prod([k ** v for k, v in factors.items() if v != 0])
 
 if __name__ == '__main__':
-    n = 1000
-    i = 10000
+    n = 20
+    i = 20000
     prob_id = 5
-    timed.caller(dummy, n, i, prob_id)
+    timed.caller(fn_prime_based, n, i, prob_id)
