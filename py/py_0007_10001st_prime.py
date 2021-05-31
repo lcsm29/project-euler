949f84116a3758e9446c114f7b7beadb5f9b1304
lcsm29 <lcsm29@outlook.com>
2021-06-01 01:50:31 +0900

    solution for problem 7 added (very slow brute force)

--- py/py_0007_10001st_prime.py
+++ py/py_0007_10001st_prime.py
@@ -9,12 +9,26 @@
 import timed
 
 
-def dummy(n):
-    pass
+def fn_brute(n):
+    assert n > 0, 'n should be greater than 0'
+    assert type(n) == int, 'n should be an integer'
+    if n in (1, 2): return n + 1
+    primes_found = 2
+    i = 3
+    while primes_found < n:
+        i += 2
+        sqrt = int(i ** 0.5)
+        for div in range(2, sqrt + 1):
+            if i % div == 0:
+                if div < sqrt: 
+                    break
+            elif div == sqrt:
+                primes_found += 1
+    return i
 
 
 if __name__ == '__main__':
-    n = 1000
-    i = 10000
+    n = 10001
+    i = 10
     prob_id = 7
-    timed.caller(dummy, n, i, prob_id)
+    timed.caller(fn_brute, n, i, prob_id)
