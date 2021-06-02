b2e362bd6aa176faa34d20ceebc900e242209404
lcsm29 <lcsm29@outlook.com>
2021-06-03 00:12:32 +0900

    #7 new sieve added but removed due to its speed

--- py/py_0007_10001st_prime.py
+++ py/py_0007_10001st_prime.py
@@ -28,8 +28,10 @@ def fn_brute(n):
     return i
 '''
 
-def fn_sieve_based(n):
-    def sieve(limit):
+def fn_sieve_eratosthenes(n):
+    assert n > 0, 'n should be greater than 0'
+    assert type(n) == int, 'n should be an integer'
+    def eratosthenes(limit):
         arr = [True] * (limit + 1)
         i = 2
         while i * i <= limit:
@@ -38,22 +40,52 @@ def fn_sieve_based(n):
                     arr[np] = False
             i += 1
         return arr
-    counter = -2
-    if n > 5:
-        limit = int(n * (log(n) + log(log(n))))
+    if n == 1:
+        return 2
+    elif 1 < n <= 5:
+        limit = n * n
     else:
-        limit = n
-    tf_table = sieve(limit)
+        limit = int(n * (log(n) + log(log(n))))
+    tf_table = eratosthenes(limit)
+    counter = -2
     for i, prime in enumerate(tf_table):
         if prime:
             counter += 1
         if counter == n:
             return i
-    
+
+''' removed due to its slow speed
+def fn_sieve_sundaram(n):
+    assert n > 0, 'n should be greater than 0'
+    assert type(n) == int, 'n should be an integer'
+    def sundaram(limit):
+        k = (limit - 2) // 2
+        arr = [True] * (k + 1)
+        for i in range(1, k + 1):
+            j = i
+            while i + j + 2 * i * j <= k:
+                arr[i + j + 2 * i * j] = False
+                j += 1
+        return arr
+    if n == 1:
+        return 2
+    elif 1 < n <= 5:
+        limit = n * n
+    else:
+        limit = int(n * (log(n) + log(log(n))))
+    tf_table = sundaram(limit)
+    counter = 1
+    for i in range(1, ((limit - 2) // 2) + 1):
+        if tf_table[i]:
+            counter += 1
+            if counter == n:
+                return 2 * i + 1
+'''    
 
 if __name__ == '__main__':
     n = 10001
     i = 100
     prob_id = 7
     # timed.caller(fn_brute, n, i, prob_id)
-    timed.caller(fn_sieve_based, n, i, prob_id)
+    timed.caller(fn_sieve_eratosthenes, n, i, prob_id)
+    # timed.caller(fn_sieve_sundaram, n, i, prob_id)
