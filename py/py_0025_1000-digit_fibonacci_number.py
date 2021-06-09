9b5ad0fe4c0276322c409d1bd4f02036a18f7ff3
lcsm29 <lcsm29@outlook.com>
2021-06-10 03:22:02 +0900

    solution for problem 25 added

--- py/py_0025_1000-digit_fibonacci_number.py
+++ py/py_0025_1000-digit_fibonacci_number.py
@@ -2,22 +2,46 @@
 # Project Euler Problem 25: 1000-digit Fibonacci number
 # https://projecteuler.net/problem=25
 # 
-# The Fibonacci sequence is defined by the recurrence relation:Fn = Fn−1 + 
-# Fn−2, where F1 = 1 and F2 = 1. Hence the first 12 terms will be:F1 = 1F2 = 
-# 1F3 = 2F4 = 3F5 = 5F6 = 8F7 = 13F8 = 21F9 = 34F10 = 55F11 = 89F12 = 144The 
-# 12th term, F12, is the first term to contain three digits. What is the index 
-# of the first term in the Fibonacci sequence to contain 1000 digits?
+# The Fibonacci sequence is defined by the recurrence relation:
+#   Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1. 
+#
+# Hence the first 12 terms will be:
+#   F1 = 1
+#   F2 = 1
+#   F3 = 2
+#   F4 = 3
+#   F5 = 5
+#   F6 = 8
+#   F7 = 13
+#   F8 = 21
+#   F9 = 34
+#   F10 = 55
+#   F11 = 89
+#   F12 = 144
+#
+# The 12th term, F12, is the first term to contain three digits. What is the
+# index of the first term in the Fibonacci sequence to contain 1000 digits?
 # 
 # by lcsm29 http://github.com/lcsm29/project-euler
 import timed
 
 
-def dummy(n):
-    pass
+def fn_recursive(n):
+    def fib(n, computed = {0: 0, 1: 1}):
+        if n not in computed:
+            computed[n] = fib(n-1, computed) + fib(n-2, computed)
+        return computed[n]
+
+    i = 0
+    while 1:
+        i += 1
+        tmp = fib(i)
+        if len(str(tmp)) >= n:
+            return i
 
 
 if __name__ == '__main__':
-    n = 1000
-    i = 10000
+    n = 1_000
+    i = 40
     prob_id = 25
-    timed.caller(dummy, n, i, prob_id)
+    timed.caller(fn_recursive, n, i, prob_id)
