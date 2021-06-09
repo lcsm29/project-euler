62e8e8168a883e135f3e499c4ac016b64fae3969
lcsm29 <lcsm29@outlook.com>
2021-06-10 03:34:42 +0900

    solution for problem 24 added (based on itertools)

--- py/py_0024_lexicographic_permutations.py
+++ py/py_0024_lexicographic_permutations.py
@@ -2,23 +2,27 @@
 # Project Euler Problem 24: Lexicographic permutations
 # https://projecteuler.net/problem=24
 # 
-# A permutation is an ordered arrangement of objects. For example, 3124 is one 
-# possible permutation of the digits 1, 2, 3 and 4. If all of the permutations 
-# are listed numerically or alphabetically, we call it lexicographic order. 
-# The lexicographic permutations of 0, 1 and 2 are:012 021 102 120 201 210What 
-# is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
-# 6, 7, 8 and 9?
+# A permutation is an ordered arrangement of objects. 
+# For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+# If all of the permutations are listed numerically or alphabetically, 
+# we call it lexicographic order. 
+# The lexicographic permutations of 0, 1 and 2 are: 012 021 102 120 201 210
+# What is the millionth lexicographic permutation of the digits 
+# 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 # 
 # by lcsm29 http://github.com/lcsm29/project-euler
 import timed
+from itertools import islice as isli
+from itertools import permutations as iperm
 
 
-def dummy(n):
-    pass
+def fn_itertools(n):
+    return int(''.join(str(i) for i in next(
+        isli(iperm(list(range(10))), n - 1, None))))
 
 
 if __name__ == '__main__':
-    n = 1000
-    i = 10000
+    n = 1_000_000
+    i = 75
     prob_id = 24
-    timed.caller(dummy, n, i, prob_id)
+    timed.caller(fn_itertools, n, i, prob_id)
