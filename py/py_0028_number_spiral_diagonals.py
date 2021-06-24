5f516b77d52694f3d3e7521459cf9fbd29c0d922
lcsm29 <lcsm29@outlook.com>
2021-06-25 03:11:54 +0900

    Add solution 28: Number spiral diagonals

--- py/py_0028_number_spiral_diagonals.py
+++ py/py_0028_number_spiral_diagonals.py
@@ -2,22 +2,29 @@
 # Project Euler Problem 28: Number spiral diagonals
 # https://projecteuler.net/problem=28
 # 
-# Starting with the number 1 and moving to the right in a clockwise direction 
-# a 5 by 5 spiral is formed as follows:21 22 23 24 2520 7 8 9 1019 6 1 2 1118 
-# 5 4 3 1217 16 15 14 13It can be verified that the sum of the numbers on the 
-# diagonals is 101. What is the sum of the numbers on the diagonals in a 1001 
-# by 1001 spiral formed in the same way?
+# Starting with the number 1 and moving to the right 
+# in a clockwise direction a 5 by 5 spiral is formed as follows:
+#
+#   21 22 23 24 25
+#   20  7  8  9 10
+#   19  6  1  2 11
+#   18  5  4  3 12
+#   17 16 15 14 13
+# 
+# It can be verified that the sum of the numbers on the diagonals is 101. 
+# What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral 
+# formed in the same way?
 # 
 # by lcsm29 http://github.com/lcsm29/project-euler
 import timed
 
 
-def dummy(n):
-    pass
+def fn_short(n):
+    return sum(i**2 + (i**2 - 2*(i-1))*3 for i in range(1, n + 1, 2)) - 3
 
 
 if __name__ == '__main__':
-    n = 1000
-    i = 10000
+    n = 1_001
+    i = 4_500
     prob_id = 28
-    timed.caller(dummy, n, i, prob_id)
+    timed.caller(fn_short, n, i, prob_id)
