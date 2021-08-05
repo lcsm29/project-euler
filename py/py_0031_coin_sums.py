762e68a71e7190bed009d707814dbb654125c818
lcsm29 <lcsm29@outlook.com>
2021-08-05 15:33:18 +0900

    Initial Commit P0031 Coin Sums

--- py/py_0031_coin_sums.py
+++ py/py_0031_coin_sums.py
@@ -3,21 +3,28 @@
 # https://projecteuler.net/problem=31
 # 
 # In the United Kingdom the currency is made up of pound (£) and pence (p). 
-# There are eight coins in general circulation:1p, 2p, 5p, 10p, 20p, 50p, £1 
-# (100p), and £2 (200p). It is possible to make £2 in the following way:1×£1 + 
-# 1×50p + 2×20p + 1×5p + 1×2p + 3×1pHow many different ways can £2 be made 
-# using any number of coins?
+# There are eight coins in general circulation:
+#   1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+# 
+# It is possible to make £2 in the following way:
+#   1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+# 
+# How many different ways can £2 be made using any number of coins?
 # 
 # by lcsm29 http://github.com/lcsm29/project-euler
 import timed
 
 
-def dummy(n):
-    pass
+def fn_brute(target, coins=(1, 2, 5, 10, 20, 50, 100, 200)):
+    arr = [1] + [0] * target
+    for coin in coins:
+        for i in range(len(arr) - coin):
+            arr[i + coin] += arr[i]
+    return arr[-1]
 
 
 if __name__ == '__main__':
-    n = 1000
-    i = 10000
+    n = 200
+    i = 15_000
     prob_id = 31
-    timed.caller(dummy, n, i, prob_id)
+    timed.caller(fn_brute, n, i, prob_id)
